10dd9593c5022313b9982c7c1a3b1a872d0b7f82
jungYonghee <69584880+bupomya@users.noreply.github.com>
2024-07-13 11:47:41 +0900

    FSM 해려고 했음
    FSM 해려고 했음

---
+++ Assets/Scripts/Player/MonsterFSMController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterFSMController : MonoBehaviour
+{
+    public enum STATE { IDLE, MOVE, ATTACK, DIE};
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
